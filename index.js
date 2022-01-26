6e0ef5ac32aba4c2cdc5d04b8f21f6d55734fd8f
aclaputra <muhamadacla22@gmail.com>
2022-01-26 20:59:32 +0700

    added all

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const products = require('./data/products');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/api/products') {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(products));
+        
     } else {
         res.writeHead(404, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({message: 'Route Not Found'}));
--- models/productModel.js
+++ models/productModel.js
@@ -1,1 +1,11 @@
- 
+const products = require('./data/products');
+
+function findAll() {
+    return new Promise((resolve, reject) => {
+        resolve(products);
+    })
+}
+
+module.export = {
+    findAll
+}

11cb4e1c29972167ec50e95d11e4269c033ac540
aclaputra <muhamadacla22@gmail.com>
2022-01-27 07:46:04 +0700

    modifed index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const http = require('http');
-const products = require('./data/products');
+const products = require('./data/productsimage');
+const productsnoimg = require('./data/products.json');
 
 const server = http.createServer((req, res) => {
     if(req.url === '/api/products') {
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(products));
+    } else if(req.url === '/api/productsnoimg') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(productsnoimg))
     } else {
         res.writeHead(404, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({message: 'Route Not Found'}));
